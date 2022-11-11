2e34509df01b23a1f097224fa1c8b4e606513862
Purvajakale <65998761+Purvajakale@users.noreply.github.com>
2022-11-12 00:18:06 +0530

    Update data-visualization.r

--- data-visualization.r
+++ data-visualization.r
@@ -5,3 +5,20 @@ plot(x,y,main="Cars", xlab="speed", ylab="distance", col="red")
 x2<-c(10,11,9,2,7,6,8,14,11,2,1)
 y2<-c(1,2,3,2,9,5,3,6,17,11,19)
 points(x2,y2,col="blue")
+
+# scatter plot
+plot(x,y,main="Cars", xlab="speed", ylab="distance", col="red")
+
+# line chart
+plot(x, type="o")
+
+# bar graph
+barplot(x, xlab = "X-axis", ylab = "Y-axis", main ="Bar-Chart", horiz=TRUE)
+
+# histogram
+hist(x, xlab = "X-axis", ylab = "Y-axis", main ="histogram")
+
+# pie chart
+geeks<- c(23, 56, 20, 63)
+labels <- c("Mumbai", "Pune", "Chennai", "Bangalore")
+pie(geeks, labels)
